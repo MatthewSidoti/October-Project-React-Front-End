dd5fa1ea228baa60c2b9c68356ff1d20624b7466
MatthewSidoti <coding.sidoti@gmail.com>
2024-10-28 19:46:53 -0400

    homepage2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import HomePage from "./components/HomePage"; // Ensure this path is correct
+import HomePage from "./components/HomePage"; 
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Root route displays the HomePage component */}
+        {}
         <Route path="/" element={<HomePage />} />
       </Routes>
     </Router>
